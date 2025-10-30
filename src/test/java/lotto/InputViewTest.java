cada887fac3a180002b2b853a90ea7f8850093f5
Hajuuu <hpt0127@naver.com>
2025-10-30 20:13:05 +0900

    test: 로또 번호 입력 검증 테스트 추가

--- src/test/java/lotto/InputViewTest.java
+++ src/test/java/lotto/InputViewTest.java
@@ -4,14 +4,20 @@ import static lotto.ErrorMessages.INPUT_AMOUNT_IS_NEGATIVE;
 import static lotto.ErrorMessages.INPUT_AMOUNT_IS_NOT_NUMBER;
 import static lotto.ErrorMessages.INPUT_AMOUNT_IS_OVER;
 import static lotto.ErrorMessages.INPUT_AMOUNT_WRONG_UNITS;
+import static lotto.ErrorMessages.INPUT_LOTTO_IS_EMPTY;
+import static lotto.ErrorMessages.INPUT_LOTTO_IS_NOT_NUMBER;
+import static lotto.ErrorMessages.INPUT_LOTTO_NUMBER_DUPLICATE;
+import static lotto.ErrorMessages.INPUT_WRONG_RANGE;
 import static lotto.InputView.validateAmount;
+import static lotto.InputView.validateLotto;
+import static lotto.InputView.validateParseInt;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class InputViewTest {
-    
+
     @Test
     @DisplayName("구매 금액이 0보다 작은 경우 예외 발생")
     void 구매_금액이_0보다_작은_경우_예외_발생() {
@@ -19,7 +25,7 @@ class InputViewTest {
         String purchase = "-10000";
 
         // when & then
-        Assertions.assertThatThrownBy(() -> validateAmount(purchase))
+        assertThatThrownBy(() -> validateAmount(purchase))
                 .hasMessage(INPUT_AMOUNT_IS_NEGATIVE.getMessage());
     }
 
@@ -30,7 +36,7 @@ class InputViewTest {
         String purchase = "150000";
 
         // when & then
-        Assertions.assertThatThrownBy(() -> validateAmount(purchase))
+        assertThatThrownBy(() -> validateAmount(purchase))
                 .hasMessage(INPUT_AMOUNT_IS_OVER.getMessage());
     }
 
@@ -41,19 +47,63 @@ class InputViewTest {
         String purchase = "1200";
 
         // when & then
-        Assertions.assertThatThrownBy(() -> validateAmount(purchase))
+        assertThatThrownBy(() -> validateAmount(purchase))
                 .hasMessage(INPUT_AMOUNT_WRONG_UNITS.getMessage());
     }
 
     @Test
     @DisplayName("숫자가 아닐 경우 예외 발생")
-    void inputAmount() {
+    void 숫자가_아닐_경우_예외_발생() {
         // given
         String purchase = "abc";
 
         // when & then
-        Assertions.assertThatThrownBy(() -> validateAmount(purchase))
+        assertThatThrownBy(() -> validateAmount(purchase))
                 .hasMessage(INPUT_AMOUNT_IS_NOT_NUMBER.getMessage());
     }
 
+    @Test
+    @DisplayName("번호가 1~45 사이의 숫자가 아닐 경우 예외 발생")
+    void 번호가_1에서_45_사이의_숫자가_아닐_경우_예외_발생() {
+        // given
+        String numbers = "0,1,2,3,4,5";
+
+        // when & then
+        assertThatThrownBy(() -> validateLotto(numbers))
+                .hasMessage(INPUT_WRONG_RANGE.getMessage());
+    }
+
+    @Test
+    @DisplayName("숫자가 아닌 문자 입력 시 예외 발생")
+    void 숫자가_아닌_문자_입력_시_예외_발생() {
+        // given
+        String numbers = "a,1,2,3,4,5";
+
+        // when & then
+        assertThatThrownBy(() -> validateParseInt(numbers))
+                .hasMessage(INPUT_LOTTO_IS_NOT_NUMBER.getMessage());
+    }
+
+    @Test
+    @DisplayName("로또에 중복된 값 입력 시 예외 발생")
+    void 로또에_중복된_값_입력_시_예외_발생() {
+        // given
+        String numbers = "1,2,3,4,4,5";
+
+        // when & then
+        assertThatThrownBy(() -> validateLotto(numbers))
+                .hasMessage(INPUT_LOTTO_NUMBER_DUPLICATE.getMessage());
+    }
+
+
+    @Test
+    @DisplayName("로또 번호에 숫자 없이 공백만 있는 경우")
+    void 로또_번호에_숫자_없이_공백만_있는_경우() {
+        // given
+        String numbers = "1, 2, ,3,4,5,6";
+
+        // when & then
+        assertThatThrownBy(() -> validateLotto(numbers))
+                .hasMessage(INPUT_LOTTO_IS_EMPTY.getMessage());
+    }
 }
