e7266a0032e0785cb32b448e3c9371bea5698baf
Hajuuu <hpt0127@naver.com>
2025-11-01 15:55:14 +0900

    test: 로또 번호 발행 로직 테스트 구현

--- src/test/java/lotto/LottoMachineTest.java
+++ src/test/java/lotto/LottoMachineTest.java
@@ -2,6 +2,7 @@ package lotto;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -19,5 +20,18 @@ class LottoMachineTest {
 
         // then
         assertThat(tickets).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("로또 번호 발행 테스트")
+    void 로또_번호_발행_테스트() {
+        // given
+        LottoMachine lottoMachine = new LottoMachine();
+
+        // when
+        List<Integer> numbers = lottoMachine.pickLottoNumbers();
+
+        // then
+        assertThat(numbers.size()).isEqualTo(6);
     }
 }
