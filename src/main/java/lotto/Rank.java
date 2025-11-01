8cc4f38f6d4917c7f1b69d9cf29dc064f4301d2c
Hajuuu <hpt0127@naver.com>
2025-11-01 16:48:49 +0900

    feat: 로또 등급을 나타내는 Enum 구현
    - 등수별 상금, 번호 일치 개수, 보너스 번호 일치 여부 정의
    - 번호 일치 개수와 보너스 번호 일치 여부를 기반으로 등수를 판정하는 findRank 메서드 추가

---
+++ src/main/java/lotto/Rank.java
@@ -0,0 +1,34 @@
+package lotto;
+
+import java.util.Arrays;
+
+public enum Rank {
+    FIRST(2_000_000_000, false, 6),
+    SECOND(30_000_000, true, 5),
+    THIRD(1_500_000, false, 5),
+    FOURTH(50_000, false, 4),
+    FIFTH(5_000, false, 3),
+    NONE(0, false, 0);
+
+    private int winnings;
+    private boolean bonus;
+    private int count;
+
+    Rank(int winnings, boolean bonus, int count) {
+        this.winnings = winnings;
+        this.bonus = bonus;
+        this.count = count;
+    }
+
+    public int getWinnings() {
+        return winnings;
+    }
+
+    public Rank findRank(int count, boolean bonus) {
+        return Arrays.stream(values())
+                .filter(rank -> rank.count == count)
+                .filter(rank -> rank.bonus == bonus)
+                .findFirst()
+                .orElse(NONE);
+    }
+}
