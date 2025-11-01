dde3ebf5836c8d903cbad44346b37c4e94fa653e
Hajuuu <hpt0127@naver.com>
2025-11-01 16:23:26 +0900

    feat: 구매 금액만큼 로또를 발행하는 로직 구현
    - 로또 발행 로직 구현
    - 번호를 뽑는 pickLottoNumbers()와 금액에 따른 개수를 구하는 buyLotto() 접근제어자 변경

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -2,16 +2,26 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LottoMachine {
 
     private static final int LOTTO_PRICE = 1000;
 
-    public int buyTickets(int amount) {
+    public List<Lotto> issuanceLotto(int amount) {
+        int number = buyLotto(amount);
+        return IntStream.range(0, number)
+                .mapToObj(num -> new Lotto(pickLottoNumbers()))
+                .collect(Collectors.toList());
+    }
+
+    private int buyLotto(int amount) {
         return amount / LOTTO_PRICE;
     }
 
-    public List<Integer> pickLottoNumbers() {
+    private List<Integer> pickLottoNumbers() {
         return Randoms.pickUniqueNumbersInRange(1, 45, 6);
     }
+
 }
