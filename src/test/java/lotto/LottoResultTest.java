3650e038debf38d6dbbd54c5c82ccb842c23fc80
Hajuuu <hpt0127@naver.com>
2025-11-01 18:27:39 +0900

    test: 로또 결과 확인 로직 검증 테스트 작성

---
+++ src/test/java/lotto/LottoResultTest.java
@@ -0,0 +1,37 @@
+package lotto;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoResultTest {
+
+    @Test
+    @DisplayName("로또 결과 확인 테스트")
+    void 로또_결과_확인_테스트() {
+        // given
+        LottoResult lottoResult = new LottoResult();
+        List<Lotto> purchaseLotto = List.of(
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)),   // 6개 일치
+                new Lotto(List.of(1, 2, 3, 4, 5, 10)),  // 5개 + 보너스 일치
+                new Lotto(List.of(1, 2, 3, 7, 8, 9))    // 3개 일치
+        );
+
+        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
+        int bonusNumber = 10;
+
+        // when
+        lottoResult.confirmWinnings(purchaseLotto, winningNumbers, bonusNumber);
+
+        // then
+        Map<Rank, Integer> results = lottoResult.getResults();
+        assertThat(results.get(Rank.FIRST)).isEqualTo(1);
+        assertThat(results.get(Rank.SECOND)).isEqualTo(1);
+        assertThat(results.get(Rank.FIFTH)).isEqualTo(1);
+
+
+    }
+}
