21331f7dff6723aa26ef6e9b41880162db608190
Hajuuu <hpt0127@naver.com>
2025-11-02 17:14:18 +0900

    feat: Lotto 생성자에 검증 로직 추가
    - 중복된 숫자 검증
    - 번호 범위(1~45) 검증

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,13 +1,32 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicate(numbers);
+        validateRange(numbers);
         this.numbers = sortedNumbers(numbers);
+    }
+
+    private static void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException(ErrorMessages.INPUT_LOTTO_NUMBER_DUPLICATE.getMessage());
+        }
+    }
+
+    private static void validateRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException(ErrorMessages.INPUT_WRONG_RANGE.getMessage());
+            }
+        }
     }
 
     private void validate(List<Integer> numbers) {
@@ -15,7 +34,6 @@ public class Lotto {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
-
 
     private List<Integer> sortedNumbers(List<Integer> numbers) {
         return numbers.stream()
