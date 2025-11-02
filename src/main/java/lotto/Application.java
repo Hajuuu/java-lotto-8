a572eaed82c785d40862169784d55f8b65f72000
Hajuuu <hpt0127@naver.com>
2025-11-02 17:01:55 +0900

    feat: main 메서드에 애플리케이션 실행 로직 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,18 @@
 package lotto;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int amount = InputView.inputAmount();
+        LottoMachine lottoMachine = new LottoMachine();
+        List<Lotto> lotto = lottoMachine.issuanceLotto(amount);
+        OutputView.printLotto(lotto);
+        List<Integer> numbers = InputView.inputWinningNumbers();
+        int number = InputView.inputBonusNumber(numbers);
+        LottoResult lottoResult = new LottoResult();
+        lottoResult.confirmWinnings(lotto, numbers, number);
+        double rate = LottoStatistics.calculateProfitRate(lottoResult, amount);
+        OutputView.printWinningStatistics(lottoResult, rate);
     }
 }
