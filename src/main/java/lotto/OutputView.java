dc4feeaac59ca0111c501d84eb4b40a296e657b2
Hajuuu <hpt0127@naver.com>
2025-11-02 16:26:55 +0900

    feat: 당첨 통계 출력 로직 구현
    - 당첨 내역과 수익률을 출력하는 printWinningStatistics() 메서드 구현
    - rank별 메시지 출력을 위해 getRankMessage() 메서드 추가
    - 일치하는 개수를 가져오기 위해 Rank에 getCount() 메서드 추가

--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -1,10 +1,34 @@
 package lotto;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class OutputView {
 
     public static void printLotto(List<Lotto> lotto) {
         lotto.forEach(System.out::println);
+    }
+
+    public static void printWinningStatistics(LottoResult lottoResult, double profitRate) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        Arrays.stream(Rank.values())
+                .filter(rank -> rank != Rank.NONE)
+                .sorted(Comparator.comparingInt(Rank::getWinnings)) // 낮은 순부터 출력
+                .forEach(rank -> {
+                    System.out.printf(
+                            "%d%s (%d원) - %d개%n", rank.getCount(), getRankMessage(rank), rank.getWinnings(),
+                            lottoResult.getCount(rank)
+                    );
+                });
+        System.out.printf("총 수익률은 %.1f%%입니다.%n", profitRate);
+    }
+
+    private static String getRankMessage(Rank rank) {
+        if (rank == Rank.SECOND) {
+            return "개 일치, 보너스 볼 일치";
+        }
+        return rank.getCount() + "개 일치";
     }
 }
--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -31,4 +31,8 @@ public enum Rank {
     public int getWinnings() {
         return winnings;
     }
+
+    public int getCount() {
+        return count;
+    }
 }
